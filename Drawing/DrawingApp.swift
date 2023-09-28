712701cf86fb5f2d0a850eb2c877617437ae4a32
Khalid Kamil <khalidkamil28@gmail.com>
2023-09-28 12:00:41 +0100

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Khalid Kamil on 28/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

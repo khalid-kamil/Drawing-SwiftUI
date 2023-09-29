d7dd8ebf556ac4fbb06f33c73c02f66d19eaae01
Khalid Kamil <khalidkamil28@gmail.com>
2023-09-29 16:25:52 +0100

    Enabling high-performance Metal rendering with drawingGroup()

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -8,22 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var petalOffset = -20.0
-    @State private var petalWidth = 100.0
+    @State private var colorCycle = 0.0
 
     var body: some View {
-        VStack {
-            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
-                .fill(.red, style: FillStyle(eoFill: true))
+            VStack {
+                ColorCyclingCircle(amount: colorCycle)
+                    .frame(width: 300, height: 300)
 
-            Text("Offset")
-            Slider(value: $petalOffset, in: -40...40)
-                .padding([.horizontal, .bottom])
-            
-            Text("Width")
-            Slider(value: $petalWidth, in: 0...100)
-                .padding([.horizontal])
-        }
+                Slider(value: $colorCycle)
+            }
     }
 }
 
@@ -33,36 +26,36 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct Flower: Shape {
-    // How much to move this petal away from the centre
-    var petalOffset: Double = -20
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    let steps = 100
 
-    // How wide to make each petal
-    var petalWidth: Double = 100
-
-    func path(in rect: CGRect) -> Path {
-        // The path that will hold all petals
-        var path = Path()
-
-        // Count from 0 to pi*2, moving up pi/8 each time
-        for number in stride(from: 0, to: Double.pi*2, by: Double.pi/8) {
-            // Rotate the petal by the current value of our loop
-            let rotation = CGAffineTransform(rotationAngle: number)
-
-            // Move the petal to be at the centre of our view
-            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width/2, y: rect.height/2))
-
-            // Create a path for this petal using our properties plus a fixed Y and height
-            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width/2))
-
-            // Apply our rotation/position transformation to the petal
-            let rotatedPetal = originalPetal.applying(position)
-
-            // Add it to our main path
-            path.addPath(rotatedPetal)
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: Double(value))
+                    .strokeBorder(
+                    LinearGradient(
+                    gradient: Gradient(colors: [
+                    color(for: value, brightness: 1),
+                    color(for: value, brightness: 0.5)
+                    ]),
+                    startPoint: .top,
+                    endPoint: .bottom
+                    ),
+                    lineWidth: 2
+                )
+            }
         }
+        .drawingGroup()
+    }
 
-        // Now send the main path back
-        return path
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(steps) + amount
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
     }
 }

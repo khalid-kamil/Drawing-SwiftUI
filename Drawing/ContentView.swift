6eba0e60f353da391649f5e46895600b7394c11d
Khalid Kamil <khalidkamil28@gmail.com>
2023-09-29 16:33:44 +0100

    Special effects in SwiftUI: blurs, blending, and more

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -8,54 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var colorCycle = 0.0
+    @State private var amount = 0.0
 
     var body: some View {
-            VStack {
-                ColorCyclingCircle(amount: colorCycle)
-                    .frame(width: 300, height: 300)
+        VStack {
+            Image("singapore")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+                .saturation(amount)
+                .blur(radius: (1 - amount) * 20)
 
-                Slider(value: $colorCycle)
+            ZStack {
+                Circle()
+                    .fill(.red)
+                    .frame(width: 200 * amount)
+                    .offset(x: -50, y: -80)
+                    .blendMode(.screen)
+
+                Circle()
+                    .fill(.green)
+                    .frame(width: 200 * amount)
+                    .offset(x: 50, y: -80)
+                    .blendMode(.screen)
+
+                Circle()
+                    .fill(.blue)
+                    .frame(width: 200 * amount)
+                    .blendMode(.screen)
             }
+            .frame(width: 300, height: 300)
+
+            Slider(value: $amount)
+                .padding()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.black)
+        .ignoresSafeArea()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
-}
-
-struct ColorCyclingCircle: View {
-    var amount = 0.0
-    let steps = 100
-
-    var body: some View {
-        ZStack {
-            ForEach(0..<steps) { value in
-                Circle()
-                    .inset(by: Double(value))
-                    .strokeBorder(
-                    LinearGradient(
-                    gradient: Gradient(colors: [
-                    color(for: value, brightness: 1),
-                    color(for: value, brightness: 0.5)
-                    ]),
-                    startPoint: .top,
-                    endPoint: .bottom
-                    ),
-                    lineWidth: 2
-                )
-            }
-        }
-        .drawingGroup()
-    }
-
-    func color(for value: Int, brightness: Double) -> Color {
-        var targetHue = Double(value) / Double(steps) + amount
-        if targetHue > 1 {
-            targetHue -= 1
-        }
-        return Color(hue: targetHue, saturation: 1, brightness: brightness)
     }
 }
